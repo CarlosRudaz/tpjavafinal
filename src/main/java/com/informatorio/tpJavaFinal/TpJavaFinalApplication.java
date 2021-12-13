5eb7d71be3e0879d5a92e342f7a1b42e40de379a
CarlosRudaz <carlos_rudaz@hotmail.com>
2021-12-13 14:13:55 -0300

    JI 0000 - Inicializando

---
+++ src/main/java/com/informatorio/tpJavaFinal/TpJavaFinalApplication.java
@@ -0,0 +1,13 @@
+package com.informatorio.tpJavaFinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpJavaFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpJavaFinalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/informatorio/tpJavaFinal/TpJavaFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.informatorio.tpJavaFinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpJavaFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
